93453b4b12c386006c42055c87dfb7254cacef28
Tim Schmidt <tisc7388@stud.hs-flensburg.de>
2022-11-10 17:31:51 +0100

    removed test.html; prepared router for static html files (not working)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import {Router} from "./router.mjs"
 import * as home from "./home.mjs"
+import * as imprint from "./imprint.mjs"
 import "normalize.css"
 import "./app.css"
 
 const router = new Router();
 
 router.registerRoute("/", {template: home.template, callback: home.init, callbackParam: [router]});
+router.registerRoute("/imprint", {template: imprint.template, callback: null, callbackParam: []});
 
 // take current pathname
 router.loadContent(window.location.pathname);
