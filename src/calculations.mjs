790b9082492c5bdf4dfaabc259118da75a1a7dd4
Tytonida3 <informatik@d-bruegge.de>
2022-11-10 13:45:57 +0100

    added impressum

--- src/calculations.mjs
+++ src/calculations.mjs
@@ -1,3 +1,5 @@
 export function cost_calculation (lastYearCosts, incrasePercentage) {
     return lastYearCosts + lastYearCosts * incrasePercentage / 100;
 }
+
+export function difference_euro(lastYearCosts, )
