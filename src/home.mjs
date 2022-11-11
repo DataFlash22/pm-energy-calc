ea82a692f6a2d86cdf2c960b07943b28e099d2a2
Pascal Ahlf <Pascal.Ahlf@stud.hs-flensburg.de>
2022-11-11 20:22:44 +0100

    Einfügen der Tabelle, bis jetzt funktioniert nur Strom.

--- src/home.mjs
+++ src/home.mjs
@@ -92,64 +92,117 @@ export function init(router) {
       prepareSlider(e, "heater");
     });
 }
-
 export const template = `
 <div class="home w3-flat-turquoise" style="padding:1rem">
     <h2 class="text-center">Willkommen auf ERB!</h2>
     <hr>
-
     <div class="container mb-3">
-        <div class="border border-dark rounded-2" style="height: 300px;"></div>
-        </div>
-    
-        <h2>Stromquelle</h2>
-    <button id="calculateElectricity" class="btn btn-warning">Stromkosten berechnen</button>
-        <label for="user-input-electricity">Vorjahresverbrauch (in kw/h):</label>
-        <input type="number" id="user-input-electricity" value="4500">
-        <input style="display:none" type="range" class="form-range" min="-100" max="100" id="range-slider-electricity">
-        <p class="text-center" id="range-slider-electricity-value"></p>
-    <div id="calc-result-electricity"></div>
-    <br/>
-    <h2> Andere Energiequellen </h2>
-    <select class="form-select" aria-label="Heizung Dropdown" id="heater-select">
-        <option id="selectEnergyValue" value="selectEnergyValue" selected>Such eine Heizquelle aus</option> 
-        <option value="gas">Gas</option>
-        <option value="oil">Öl</option>
-        <option value="districtHeating">Fernwärme</option>
-        <option value="heatPump">Wärmepumpe</option>
-        <option value="woodPellets">Holzpellets</option>
-      </select>
-    </br>
-    <button id="calculateHeater" class="btn btn-danger">Heizkosten berechnen</button>
-    <label for="user-input-heater">Vorjahresverbrauch (in kw/h):</label>
-    <input type="number" id="user-input-heater" value="4500">
-    <input style="display:none" type="range" class="form-range" min="-100" max="100" id="range-slider-heater">
-    <p class="text-center" id="range-slider-heater-value"></p>
-    <h4 class="text-center w3-flat-pomegranate" style="display:none;" id="errorHandling">Bitte wähl eine Energiequelle aus.</h4>
-
-    <div id="calc-result-heater"></div>
-
+      <div class="border rounded-2" style="padding: 5px 15px 5px 15px">
+         <h2>Stromquelle</h2>
+          <button id="calculateElectricity" class="btn btn-warning">Stromkosten berechnen</button>
+          <label for="user-input-electricity">Vorjahresverbrauch (in kw/h):</label>
+          <input type="number" id="user-input-electricity" value="4500">
+          <input style="display:none" type="range" class="form-range" min="-100" max="100" id="range-slider-electricity">
+          <p class="text-center" id="range-slider-electricity-value"></p>
+          <div class="table-responsive" style="display:none" id="table-electricity">
+            <table class="table table-light table-borderless" style="text-align: center;">
+              <thead>
+                <tr>
+                  <th scope="col">2021</th>
+                  <th scope="col">Differenz €/%</th>
+                  <th scope="col">2022</th>
+                  <th scope="col">Differenz €/%</th>
+                  <th scope="col">2023</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td rowspan="2" style="vertical-align: middle"><div id="calc-result-electricity2021"></div></td> 
+                  <td>500</td>
+                  <td rowspan="2" style="vertical-align: middle"><div id="calc-result-electricity2022"></div></td>
+                  <td>750</td>
+                  <td rowspan="2" style="vertical-align: middle"><div id="calc-result-electricity"></div></td>
+                </tr>
+                <tr>
+                  <td>50%</td>
+                  <td>50%</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+      </div>
+      <br />
+      <div class="border rounded-2" style="padding: 5px 15px 5px 15px">
+        <h2> Andere Energiequellen </h2>
+        <select class="form-select" style="width: 250px !important" aria-label="Heizung Dropdown" id="heater-select">
+          <option id="selectEnergyValue" value="selectEnergyValue" selected>Such eine Heizquelle aus</option> 
+          <option value="gas">Gas</option>
+          <option value="oil">Öl</option>
+          <option value="districtHeating">Fernwärme</option>
+          <option value="heatPump">Wärmepumpe</option>
+          <option value="woodPellets">Holzpellets</option>
+        </select>
+        <br />
+        <button id="calculateHeater" class="btn btn-danger">Heizkosten berechnen</button>
+        <label for="user-input-heater">Vorjahresverbrauch (in kw/h):</label>
+        <input type="number" id="user-input-heater" value="4500">
+        <input style="display:none" type="range" class="form-range" min="-100" max="100" id="range-slider-heater">
+        <p class="text-center" id="range-slider-heater-value"></p>
+        <h4 class="text-center w3-flat-pomegranate" style="display:none;" id="errorHandling">Bitte wähl eine Energiequelle aus.</h4>
+        <div class="table-responsive" style="display:none" id="table-heater">
+        <table class="table table-light table-borderless" style="text-align: center;">
+          <thead>
+            <tr>
+              <th scope="col">2021</th>
+              <th scope="col">Differenz €/%</th>
+              <th scope="col">2022</th>
+              <th scope="col">Differenz €/%</th>
+              <th scope="col">2023</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td rowspan="2" style="vertical-align: middle">10</td> 
+              <td>500</td>
+              <td rowspan="2" style="vertical-align: middle">20</td>
+              <td>750</td>
+              <td rowspan="2" style="vertical-align: middle"><div id="calc-result-heater"></div></td>
+            </tr>
+            <tr>
+              <td>50%</td>
+              <td>50%</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+</div>
 `;
 
-// This function show the Slider under the button if it pressed.
+// This function show the Slider and Table under the button if it pressed.
 function showSliderElectricity(showSlider) {
   showSlider = document.getElementById("range-slider-electricity").style;
-  if ((showSlider.cssText = "none")) {
+  let showTable = document.getElementById("table-electricity").style;
+  if (showSlider.cssText && showTable.cssText == "none") {
     showSlider.cssText = "block";
+    showTable.cssText = "block";
   } else {
     showSlider.cssText = "none";
+    showTable.cssText = "none";
   }
 }
 
+// This function show the Slider and Table under the button if it pressed.
 function showSliderHeater(showSlider, showError) {
   showSlider = document.getElementById("range-slider-heater").style;
+  let showTable = document.getElementById("table-heater").style;
   document.getElementById("errorHandling").style.display = "none";
-  console.log(showError);
-  if ((showSlider.cssText = "none")) {
+  if (showSlider.cssText && showTable.cssText == "none") {
     showSlider.cssText = "block";
+    showTable.cssText = "block";
   } else {
     showSlider.cssText = "none";
+    showTable.cssText = "none";
   }
 }
 
@@ -192,11 +245,17 @@ function calculateYearCosts(type, resultType) {
 
   // calculation of the difference between the years
   // Difference from 2021 to 2022
-  let difEuro2122 = differenceEuro(data.cost[2021][type], data.cost[2022][type]);
+  let difEuro2122 = differenceEuro(
+    data.cost[2021][type],
+    data.cost[2022][type]
+  );
   let difPerc2122 = differencePercent(difEuro2122, data.cost[2021][type]);
 
   // Difference from 2022 to 2023
-  let difEuro2223 = differenceEuro(data.cost[2022][type], data.cost[2023][type]);
+  let difEuro2223 = differenceEuro(
+    data.cost[2022][type],
+    data.cost[2023][type]
+  );
   let difPerc2223 = differencePercent(difEuro2223, data.cost[2021][type]);
 
   // TODO: Add difference into HTML
@@ -206,11 +265,20 @@ function calculateYearCosts(type, resultType) {
   // create 3 elements for 3 years
   for (let [key, value] of Object.entries(data.cost)) {
     let newElement = document.createElement("p");
+    //if (data.cost[2021][type] == data.cost[2021][electricity]) {
+    //console.log("Ja");
+    //}
+    console.log(data.cost[2021]);
+    newElement.className = "p" + key;
     newElement.textContent = `${value[type]}€`;
-
     newElements.push(newElement);
   }
-
+  document
+    .getElementById("calc-result-electricity2021")
+    .replaceChildren(data.cost[2021].electricity);
+  document
+    .getElementById("calc-result-electricity2022")
+    .replaceChildren(data.cost[2022].electricity);
   document
     .getElementById("calc-result-" + resultType)
     .replaceChildren(...newElements);
@@ -249,13 +317,18 @@ function calculate2023Slider(value, type, resultType) {
   data.cost[2023][type] = calculation;
 
   // calculating the new difference
-  let difEuro2223 = differenceEuro(data.cost[2022][type], data.cost[2023][type]);
+  let difEuro2223 = differenceEuro(
+    data.cost[2022][type],
+    data.cost[2023][type]
+  );
   let difPerc2223 = differencePercent(difEuro2223, data.cost[2021][type]);
 
   // TODO: Add difference into HTML
 
   // take last child (should be the value of 2023)
-  let lastChild = document.getElementById('calc-result-'+resultType).lastChild;
+  let lastChild = document.getElementById(
+    "calc-result-" + resultType
+  ).lastChild;
 
   // replace textContent with new calculation
   lastChild.textContent = `${calculation}€`;
